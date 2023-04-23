ff6a2612830f8a2415a6a15c519e228731a70cda
unknown <hamdymohame7@gmail.com>
2023-04-23 02:54:32 +0200

    v1 dynamic cash

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,43 @@
 self.addEventListener("install", function (event) {
-  console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
     caches.open("static").then((cache) => {
-      console.log("Opened cache");
-      return cache.add("./src/js/app.js");
+      cache.addAll([
+        "/",
+        "./index.html",
+        "./src/js/app.js",
+        "./src/js/feed.js",
+        "./src/js/promise.js",
+        "./src/js/fetch.js",
+        "./src/js/material.min.js",
+        "./src/css/app.css",
+        "./src/css/feed.css",
+        "./src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
     })
   );
 });
 
 self.addEventListener("activate", function (event) {
-  console.log("[Service Worker] Activating Service Worker ....", event);
   return self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log("[Service Worker] fetch Service Worker ....", event);
-
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
         return response;
       } else {
-        return fetch(event.request);
+        return fetch(event.request).then((res) => {
+          return caches.open("dynamic").then((cache) => {
+            if (event.request.url.startsWith("http")) {
+              cache.put(event.request, res.clone());
+            }
+            return res;
+          });
+        });
       }
     })
   );
